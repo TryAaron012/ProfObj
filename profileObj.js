5c92b8d77e9271034c52e24ddee9a2a2152ef23e
Aaron Raymund Paguirigan <paguiriganaaronraymund@gmail.com>
2024-11-04 21:26:13 +0800

    initial commit

---
+++ profileObj.js
@@ -0,0 +1,8 @@
+const userProfile = {
+    fullName: 'Aaron Raymund R. Paguirigan',
+    age: 19,
+    favoriteNumber: 7,
+    favoriteColors: ['Black', 'Blue', 'Red']
+  };
+  
+  console.log(userProfile);
